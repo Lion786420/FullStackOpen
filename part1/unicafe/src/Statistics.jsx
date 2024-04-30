d9447f1a46895eaeb4393af5062ff35ed6213836
Lion786420 <thapaaniket12345@gmail.com>
2024-04-30 18:02:23 +0545

    Unicafe project compete with statistic table implementation

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -5,18 +5,22 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <h1>Statistics</h1>
-      <StatisticLine text="Good" value={good} />
-      <StatisticLine text="Neutral" value={neutral} />
-      <StatisticLine text="Bad" value={bad} />
-      <StatisticLine text="All" value={good + bad + neutral} />
-      <StatisticLine
-        text="Average"
-        value={(good * 1 + neutral * 0 - bad * 1) / (good + bad + neutral)}
-      />
-      <StatisticLine
-        text="Positive"
-        value={(good / (good + bad + neutral)) * 100 + "%"}
-      />
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="All" value={good + bad + neutral} />
+          <StatisticLine
+            text="Average"
+            value={(good * 1 + neutral * 0 - bad * 1) / (good + bad + neutral)}
+          />
+          <StatisticLine
+            text="Positive"
+            value={(good / (good + bad + neutral)) * 100 + "%"}
+          />
+        </tbody>
+      </table>
     </>
   );
 };
