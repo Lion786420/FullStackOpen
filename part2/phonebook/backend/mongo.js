b965a2471042bc2e58ead6eaf2b9415d2c65305f
Lion786420 <thapaaniket12345@gmail.com>
2024-05-08 12:17:36 +0545

    Added mongo test file in phonebook directory

---
+++ part2/phonebook/backend/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://aniket_thapa:${password}@cluster0.63qntc8.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const phoneSchema = {
+  name: String,
+  number: String,
+};
+
+const Contact = mongoose.model("Contact", phoneSchema);
+
+if (process.argv.length == 3) {
+  Contact.find({}).then((result) => {
+    console.log("Phonebook:");
+    result.forEach((contact) => {
+      console.log(`${contact.name}  ${contact.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+  const contact = new Contact({ name: name, number: number });
+  contact.save().then((result) => {
+    console.log(`Added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
