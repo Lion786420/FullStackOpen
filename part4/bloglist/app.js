977fe787c4f2287a27838ae9d3f58f7cab210b12
Lion786420 <thapaaniket12345@gmail.com>
2024-05-12 21:29:39 +0545

    Made blogs backend with node good practices format applied

---
+++ part4/bloglist/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
+const blogsRouter = require("./controller/blogs");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then((result) => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((error) => {
+    logger.info("Error connecting to db");
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
