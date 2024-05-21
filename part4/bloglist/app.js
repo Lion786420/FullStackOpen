4373a1a89bf0327d7561ee25f87c636f2d2751b1
Lion786420 <thapaaniket12345@gmail.com>
2024-05-21 20:49:09 +0545

    Added login route for token based authentication

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const middleware = require("./utils/middleware");
 const blogsRouter = require("./controller/blogs");
 const usersRouter = require("./controller/users");
+const loginRouter = require("./controller/login");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
