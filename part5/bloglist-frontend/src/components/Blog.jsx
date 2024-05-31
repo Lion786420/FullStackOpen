7fc99c96c8b2efba7edb2c432301a96e3f4fff12
Lion786420 <thapaaniket12345@gmail.com>
2024-05-31 20:03:10 +0545

    Added blog detail button to each blog and added simple style to it

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false);
+  if (show) {
+    return (
+      <div className="eachBlog">
+        <div>
+          {blog.title}
+          <button onClick={() => setShow(false)}>Hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button>Like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    );
+  }
+  return (
+    <div className="eachBlog">
+      {blog.title} {blog.author}
+      <button onClick={() => setShow(true)}>View</button>
+    </div>
+  );
+};
+
+export default Blog;
