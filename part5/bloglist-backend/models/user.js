1189d399592c759e919909d4cd22ed6d23942da9
Lion786420 <thapaaniket12345@gmail.com>
2024-11-19 20:57:37 +0545

    Made small model change

--- part5/bloglist-backend/models/user.js
+++ part5/bloglist-backend/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
-  username: { type: String, required: true, unique: true, minLength: 3 },
+  username: { type: String, required: true, unique: true, minLength: 4 },
   passwordHash: String,
   blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Blog" }],
 });
