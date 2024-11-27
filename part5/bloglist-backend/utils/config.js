976a05edb8fd084c9ee14ce034d9ea8513848b45
Lion786420 <thapaaniket12345@gmail.com>
2024-11-27 21:14:54 +0545

    Salt rounds added on env

--- part5/bloglist-backend/utils/config.js
+++ part5/bloglist-backend/utils/config.js
@@ -5,5 +5,6 @@ const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
+const SALT_ROUNDS = +process.env.SALT_ROUNDS;
 
-module.exports = { PORT, MONGODB_URI };
+module.exports = { PORT, MONGODB_URI, SALT_ROUNDS };
