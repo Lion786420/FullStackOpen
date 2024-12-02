1793568c6eedff6d2f3e8b28ddbaa1eb6ef7ca22
Lion786420 <thapaaniket12345@gmail.com>
2024-12-02 06:02:02 +0545

    Added login in frontend with local storage

--- part5/bloglist-front/src/App.jsx
+++ part5/bloglist-front/src/App.jsx
@@ -1,24 +1,81 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+    const local = JSON.parse(localStorage.getItem("user"));
+    if (local !== null) {
+      setUser(local);
+    }
+  }, []);
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    const cred = { username, password };
+    const token = await loginService.login(cred);
+    console.log(token);
+    setUser(token);
+    localStorage.setItem("user", JSON.stringify(token));
+    setPassword("");
+    setUsername("");
+  };
+
+  const logoutHandler = () => {
+    localStorage.clear();
+    setUser(null);
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={loginHandler}>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <div>
+        <p>{user.name} logged in</p>
+        <button onClick={logoutHandler}>Logout</button>
+      </div>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
